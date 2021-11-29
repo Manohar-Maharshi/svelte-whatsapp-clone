4e941ba09715a210fa4a1f131ff6eb7fb0d47504
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-29 13:59:02 +0530

    🔰[UPDATE] - formatted code - left/right border

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,7 @@
-<script>import "../app.css";</script>
-<slot></slot>
+<script>
+	import "../app.css";
+</script>
+
+<main class="md:border-l md:border-r md:border-gray-800 container mx-auto min-h-screen max-w-screen-md">
+	<slot />
+</main>
