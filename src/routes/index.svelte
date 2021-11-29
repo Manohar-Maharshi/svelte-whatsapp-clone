bc6d66975556dcbd6ae737465a2fe80e1e2aab87
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-29 13:56:41 +0530

    🍱[UPDATE] - removed and simplified main components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,9 @@
 <script>
-	import SideBar from '$lib/components/SideBar.svelte'
-	import ChatContainer from '$lib/components/ChatContainer.svelte'
+	import TopNav from '$lib/components/TopNav.svelte'
+	import ChatList from '$lib/components/ChatList.svelte'
 </script>
-<main class="w-full flex items-start">
-	<aside class="w-[30%]">
-		<SideBar />
-	</aside>
-	<aside class="w-[70%]">
-		<ChatContainer />
-	</aside>
-</main>
+
+<section>
+	<TopNav />
+	<ChatList />
+</section>
